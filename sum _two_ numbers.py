d680062b897a6394ff622b50f3419ee1430364cb
Fatema Alahmad <fatemaalahmad999@gmail.com>
2025-07-20 20:11:45 +0300

    Initial commit - add sum_two_numbers.py

---
+++ sum _two_ numbers.py
@@ -0,0 +1,9 @@
+# Simple program to calculate the sum of two numbers
+
+def sum_two_numbers(number1,number2):
+    return number1+number2
+if True:
+    num1 = int(input("Enter the first number: "))
+    num2 = int(input("Enter the second number: "))
+    result = sum_two_numbers(num1, num2)
+    print(f"The sum of the two numbers is: {result}")
